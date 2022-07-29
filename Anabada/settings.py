9b54353b52b6a5bc8bdf31b81783090569c87b30
kimsungkwang <skk405@naver.com>
2022-07-29 16:19:26 +0900

    사용자 모델 만들기

--- Anabada/settings.py
+++ Anabada/settings.py
@@ -134,3 +134,6 @@ MEDIA_URL = '/media/' # 항상 / 로 끝나도록 설정
 # MEDIA_URL = 'http://static.myservice.com/media/' 다른 서버로 media 파일 복사시
 # 업로드된 파일을 저장할 디렉토리 경로
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# 커스텀 유저 모델 사용
+AUTH_USER_MODEL = "user.User"
--- content/models.py
+++ content/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Feed(models.Model):
     # 상품 명
