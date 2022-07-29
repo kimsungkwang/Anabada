fa8b9946dea9e972e2f4ed30dea1b3682bcf64b3
kimsungkwang <skk405@naver.com>
2022-07-30 01:13:30 +0900

    회원가입 창 만들기

--- Anabada/urls.py
+++ Anabada/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .view import Sub
 from content.views import Main, UploadFeed
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,7 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
-    path('content/upload', UploadFeed.as_view()),
+    path('content/', include('content.urls')),
+    path('user/', include('user.urls'))
+
 ]
 
 
