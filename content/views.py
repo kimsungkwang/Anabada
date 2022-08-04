bbeeba122a67bc27650c0231bd904befa7eff440
kimsungkwang <skk405@naver.com>
2022-08-04 11:27:25 +0900

    로그아웃, 프로필 페이지, 프로필 사진 변경

--- content/views.py
+++ content/views.py
@@ -4,8 +4,11 @@ from uuid import uuid4
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from user.models import User
 from .models import Feed
 from Anabada.settings import MEDIA_ROOT
+
 
 class Main(APIView):
     def get(self, request):
@@ -13,14 +16,24 @@ class Main(APIView):
         # select * from content_feed; 역활
         feed_list = Feed.objects.all().order_by('-id')
 
+        # 세션 --> 로그인 을 한 후 다른 웹 사이트 갔다 와도 그대로 저장 , 로그 아웃 하면 정보들 날라감
+        email = request.session.get('email', None)
 
-        return render(request, "Anabada/main.html", context=dict(feeds= feed_list))
+        if email is None:
+            return render(request, 'user/login.html')
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return render(request, 'user/login.html')
+
+        return render(request, "Anabada/main.html", context=dict(feeds=feed_list, user=user))
+
 
 class UploadFeed(APIView):
-    def post(self, requset):
-
+    def post(self, request):
         # 파일 불러오기
-        file = requset.FILES['file']
+        file = request.FILES['file']
 
         uuid_name = uuid4().hex
         save_path = os.path.join(MEDIA_ROOT, uuid_name)
@@ -31,12 +44,29 @@ class UploadFeed(APIView):
                 destination.write(chunk)
 
         image = uuid_name
-        name = requset.data.get("name")
-        price = requset.data.get("price")
-        content = requset.data.get("content")
-        user_id = requset.data.get("user_id")
-        profile_image = requset.data.get("profile_image")
+        name = request.data.get("name")
+        price = request.data.get("price")
+        content = request.data.get("content")
+        user_id = request.data.get("user_id")
+        profile_image = request.data.get("profile_image")
 
-        Feed.objects.create(image=image, name=name, price=price, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+        Feed.objects.create(image=image, name=name, price=price, content=content, user_id=user_id,
+                            profile_image=profile_image, like_count=0)
 
         return Response(status=200)
+
+
+class Profile(APIView):
+    def get(self, request):
+
+        email = request.session.get('email', None)
+
+        if email is None:
+            return render(request, 'user/login.html')
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return render(request, 'user/login.html')
+
+        return render(request, "content/profile.html", context=dict(user=user))
--- user/views.py
+++ user/views.py
@@ -1,6 +1,11 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from Anabada.settings import MEDIA_ROOT
 from .models import User
 from django.contrib.auth.hashers import make_password
 
@@ -20,7 +25,7 @@ class Join(APIView):
                             password=make_password(password),
                             name=name,
                             nickname=nickname,
-                            profile_image='default_profile.jpg')
+                            profile_image='default_profile.jpeg')
 
         return Response(status=200)
 
@@ -40,6 +45,40 @@ class Login(APIView):
 
         if user.check_password(password):
             # TODO 로그인을 했다 세션 or 쿠키
+            request.session['email'] = email
             return Response(status=200)
         else:
-            return Response(status=400, data=dict(message="비밀번호가 틀렸습니다."))
+            return Response(status=400, data=dict(message="회원 정보가 잘못되었습니다."))
+
+
+class LogOut(APIView):
+    def get(self, request):
+
+        # 세션 지우기
+        request.session.flush()
+        return render(request, 'user/login.html')
+
+class UploadProfile(APIView):
+    def post(self, request):
+
+        # 파일 불러오기
+        file = request.FILES['file']
+        email = request.data.get('email')
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        # 파일 저장
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        profile_image = uuid_name
+
+        user = User.objects.filter(email=email).first()
+
+        user.profile_image = profile_image
+        user.save()
+
+        return Response(status=200)
+
