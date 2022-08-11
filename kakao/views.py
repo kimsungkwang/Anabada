8831539482ebd5b62e227587de44f39e1e84ec9b
kimsungkwang <skk405@naver.com>
2022-08-12 03:21:21 +0900

    app key 지우고 다시 올리기 ;;

--- kakao/views.py
+++ kakao/views.py
@@ -9,7 +9,7 @@ class KakaoLoginView(APIView):
     def get(self, request):
         # kakao code 요청
         kakao_auth_uri = "https://kauth.kakao.com/oauth/authorize?response_type=code"
-        client_id = "7e3239b54ab4208760ff31bb0f3d67ed"
+        client_id = ""
         redirect_uri = "http://127.0.0.1:8000/kakao/oauth/callback"
         uri = f'{kakao_auth_uri}&client_id={client_id}&redirect_uri={redirect_uri}'
 
@@ -24,7 +24,7 @@ class KakaoCallbackView(APIView):
         kakao_token_api = 'https://kauth.kakao.com/oauth/token'
         data = {
             'grant_type': 'authorization_code',
-            'client_id': "7e3239b54ab4208760ff31bb0f3d67ed",
+            'client_id': "",
             'redirect_uri': "http://127.0.0.1:8000/kakao/oauth/callback",
             'code': auth_code
         }
